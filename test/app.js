9f2e044da024843e55d1a31724e9f8c052545dce
Thiago Santana <thiago.santana@gmail.com>
2017-11-16 18:35:59 -0200

    initial commit

---
+++ test/app.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert;
+const app = require('../app');
+
+describe('Calc', () => {
+    it('test add', () => {
+        let result = app.add(2, 2);
+        assert.equal(result, 4);
+    });
+    it('test sub', () => {
+        let result = app.sub(2, 2);
+        assert.equal(result, 0);
+    });
+    it('test mul', () => {
+        let result = app.mul(2, 2);
+        assert.equal(result, 4);
+    });
+    it('test div', () => {
+        let result = app.div(2, 2);
+        assert.equal(result, 1);
+    });
+})
